466077c1b7729544ca191a94ea81a9655ecdcb07
RobertBrown111 <151895914+RobertBrown111@users.noreply.github.com>
2024-08-25 23:45:16 +0700

    Test math

--- src/firstjava/HelloWorld.java
+++ src/firstjava/HelloWorld.java
@@ -1,10 +1,15 @@
 package firstjava;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World with Java");
 		System.out.println("Hello World with Java");
 		System.out.println("Hello World with Java");
 		System.out.println("Hello World with Java");
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		System.out.println("Sum: " + c);
 	}
 }
